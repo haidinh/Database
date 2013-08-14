5f7d2362dee9ecb1a43c44af89991cc6ed7ee074
datpt <datpt.11.1.1992@gmail.com>
2013-08-14 10:47:38 +0700

    update

--- Database/eProjectSEM3.sql
+++ Database/eProjectSEM3.sql
@@ -7,15 +7,24 @@ create table tblCareerer -- the table holds job seeker's info
 	CareererID int identity(1,1) primary key,
 	EmailAddress varchar(100) unique,
 	[Password] varchar(20),
+	[UserName] varchar(100),
 	FullName varchar(100),
 	Age int,
 	Gender bit,
 	[Address] varchar(50),
 	City varchar(50),
 	Nation varchar(50),
-	CV varchar(100), --Careerer's resume'
-	
+	CV varchar(100), 
+	Education varchar(500)--Careerer's resume'
 )
+
+--ALTER TABLE tblCareerer
+--ADD [UserName] varchar(100)
+--go
+--ALTER TABLE tblCareerer
+--ADD Education varchar(500)
+--go
+
 create table tblAdmin
 (
 	AdminID int identity(1,1) primary key,
@@ -24,6 +33,7 @@ create table tblAdmin
 	Email varchar(100),
 	Name nvarchar(100)
 )
+
 create table tblEmail2Careerer -- the table holds info that admin had sent to Careerer
 (
 	Email2CareererID int identity(1,1) primary key,
@@ -49,16 +59,15 @@ create table tblGuestFeedBack -- the table that contains feedback's content
 	FeedBackID int identity(1,1) primary key,
 	Email varchar(100),
 	FullName varchar(100),
-	EmailAddress varchar(100),
 	CompanyName varchar(100),
 	[Address] varchar(50),
 	City varchar(50),
 	Nation varchar(50),
 	PhoneNumber varchar(50),
 	PostalCode varchar(20),
+	[Subject] varchar(100),
 	Comment varchar(500)
-)
-
+) 
 
 create table tblCategory
 (
@@ -94,8 +103,9 @@ select * from tblGuestFeedBack
 --where ProductID=1
 -- INSERT
 -- admin
-insert into tblAdmin values('Admin1','1234')
-insert into tblAdmin values('Admin2','1234')
+insert into tblAdmin values('Admin','admin','datpt@gmail.com','Dat Pham Tien')
+insert into tblCareerer(EmailAddress,Password) values('nguyenmanh@gmail.com','1234')
+insert into tblCareerer(EmailAddress,Password) values('trung@gmail.com','1234')
 -- careerer
 --insert into tblCareerer(EmailAddress,[Password]) values('hung@gmail.com','1234')
 --insert into tblCareerer(EmailAddress,[Password]) values('mai@gmail.com','1234')
@@ -121,35 +131,8 @@ insert into tblProduct(CategoryID,ProductName,[Output],MadeGoodsSize,MachineSize
 insert into tblProduct(CategoryID,ProductName,[Output],MadeGoodsSize,MachineSize,[Weight]) values(3,'Fil1',400,4,'5x2x1',5000)
 insert into tblProduct(CategoryID,ProductName,[Output],MadeGoodsSize,MachineSize,[Weight]) values(3,'Fil2',200,4,'8x26x37',8800)
 
-GO
--- STORED PROCEDURE
---CREATE PROCEDURE sp_InsertPicture @Picture varchar(100),@ProductName varchar(100)
---AS
---UPDATE tblProduct SET Picture=@Picture WHERE ProductName=@ProductName
-GO
--- Stored procedure for get all category
-CREATE PROCEDURE sp_getAllCategory
-AS
-SELECT * from tblCategory
-GO
-exec sp_getAllCategory
 go
--- Stored procedure for get products viewed most frequently
-CREATE PROC sp_GetProductByFrequentlyViewed
-AS
-select TOP 6[ProductName],[Output],MadeGoodsSize,MachineSize,[Weight], [UnitPrice],[Picture],FrequentlyViewed,ProductID,[ExtraDescription] from dbo.tblProduct order by FrequentlyViewed DESC
-GO
-
-exec sp_GetProductByFrequentlyViewed
-go
-
-CREATE PROC sp_GetProductByAddedDate 
-AS
-select TOP 3[ProductName],[Output],MadeGoodsSize,MachineSize,[Weight], [UnitPrice],[Picture],FrequentlyViewed,ProductID,[ExtraDescription] from dbo.tblProduct order by AddedDate DESC
-GO
-exec sp_GetProductByAddedDate
-go
-
+--- Procduce
 -- procduce Admin
 Create Proc sp_AdminSelectAll 
 AS
@@ -222,7 +205,7 @@ END
 go
 
 Create Proc sp_CategoryInsert
-	@Category nvarchar(50)	
+	@Category nvarchar(50)
 AS
 BEGIN
 	Insert into tblCategory values (@Category) 
@@ -231,10 +214,10 @@ GO
 
 Create Proc sp_CategoryUpdate
 	@CategoryName nvarchar(50),
-	@CategoryID int
+	@CategoryId int
 AS
 BEGIN
-	Update tblCategory set CategoryName = @CategoryName where CategoryID = @CategoryID
+	Update tblCategory set CategoryName = @CategoryName where CategoryID=@CategoryId
 END
 Go
 
@@ -275,15 +258,17 @@ CREATE PROC sp_CareererInsert
 	@EmailAddress varchar(100),
 	@Password varchar(20),
 	@FullName varchar(100),
+	@Username varchar(100),
 	@Age int,
 	@Gender bit,
 	@Address varchar(50),
 	@City varchar(50),
 	@Nation varchar(50),
-	@CV varchar(100)
+	@CV varchar(100),
+	@Education varchar(500)
 AS
 Begin
-	Insert into tblCareerer values (@EmailAddress,@Password,@FullName,@Age,@Gender,@Address,@City,@Nation,@CV)
+	Insert into tblCareerer values (@EmailAddress,@Password,@FullName,@Username,@Age,@Gender,@Address,@City,@Nation,@CV,@Education)
 End
 GO
 
@@ -321,3 +306,168 @@ BEGIN
 END
 GO
 	
+-- Procduce Product
+Create Proc sp_ProductSelectAll
+AS
+BEGIN
+	Select * from tblProduct 
+END
+GO
+
+Create Proc sp_ProductSelectByID
+	@ProductID int
+AS
+BEGIN
+	Select * from tblProduct where ProductID = @ProductID
+END
+Go
+
+Create Proc sp_ProductSelectByCategory
+	@CategoryID int
+AS
+Begin
+	Select * from tblProduct where CategoryID= @CategoryID
+End
+Go
+
+Create Proc sp_ProductInsert
+	@CategoryID int,
+	@ProductName varchar(100),
+	@Output int, 
+	@MadeGoodsSize float,
+	@MachineSize varchar(50),
+	@Weight float,
+	@Picture varchar(100),
+	@UnitPrice money,
+	@ExtraDescription varchar(300)
+AS
+Begin
+	Insert into tblProduct(CategoryID,ProductName,[Output],MadeGoodsSize,MachineSize,[Weight],Picture,UnitPrice,ExtraDescription)  
+	values (@CategoryID,@ProductName,@Output,@MadeGoodsSize,@MachineSize,@Weight,@Picture,@UnitPrice,@ExtraDescription) 
+End
+Go
+
+alter Proc sp_ProductUpdate
+	@ProductID int,
+	@CategoryID int,
+	@ProductName varchar(100),
+	@Output int, 
+	@MadeGoodsSize float,
+	@MachineSize varchar(50),
+	@Picture varchar(100),
+	@Weight float,
+	@UnitPrice money,
+	@ExtraDescription varchar(300)
+As
+Begin
+	Update tblProduct set [CategoryID]= @CategoryID, [ProductName] = @ProductName,[Output] = @Output, MadeGoodsSize = @MadeGoodsSize,[MachineSize] = @MachineSize,[Weight] = @Weight,
+						 [Picture] = @Picture,[UnitPrice] = @UnitPrice,[ExtraDescription]= @ExtraDescription
+	where ProductID =@ProductID
+End
+Go
+
+Create Proc sp_ProductDelete
+	@ProductID int
+AS
+Begin
+	Delete from tblProduct where [ProductID] = @ProductID
+End
+Go
+
+-- procduce tblGuestFeedBack
+Create Proc sp_GuestFeedBackSelectAll
+AS
+Begin
+	Select * from  tblGuestFeedBack
+End
+Go
+
+Create Proc sp_GuestFeedBackSelectByEmail
+	@Email varchar(100)
+As
+Begin
+	Select * from tblGuestFeedBack where Email like '%'+@Email+'%'
+End
+Go
+
+Create Proc sp_GuestFeedBackSelectByID
+	@FeedBackID varchar(100)
+As
+Begin
+	Select * from tblGuestFeedBack where FeedBackID =@FeedBackID
+End
+Go
+
+-- STORED PROCEDURE
+CREATE PROCEDURE sp_InsertPicture @Picture varchar(100),@ProductName varchar(100)
+AS
+UPDATE tblProduct SET Picture=@Picture WHERE ProductName=@ProductName
+GO
+-- Stored procedure for get all category
+CREATE PROCEDURE sp_getAllCategory
+AS
+SELECT * from tblCategory
+GO
+
+-- Stored procedure for get products viewed most frequently
+CREATE PROC sp_GetProductByFrequentlyViewed
+AS
+select TOP 6[ProductName],[Output],MadeGoodsSize,MachineSize,[Weight], [UnitPrice],[Picture],FrequentlyViewed,ProductID,[ExtraDescription] from dbo.tblProduct order by FrequentlyViewed DESC
+GO
+
+
+CREATE PROC sp_GetProductByAddedDate 
+AS
+select TOP 3[ProductName],[Output],MadeGoodsSize,MachineSize,[Weight], [UnitPrice],[Picture],FrequentlyViewed,ProductID,[ExtraDescription] from dbo.tblProduct order by AddedDate DESC
+GO
+
+
+CREATE PROCEDURE sp_Login @EmailAddress varchar(100),@Password varchar(20)
+AS
+SELECT [UserName],[Password],CareererID from tblCareerer where EmailAddress=@EmailAddress and [Password]=@Password
+GO
+
+
+-- sign up
+CREATE PROCEDURE sp_SignUp @EmailAddress varchar(100),@Password varchar(20),@UserName varchar(100)
+AS
+insert into tblCareerer(EmailAddress,[Password],UserName) values(@EmailAddress,@Password,@UserName)
+GO
+
+-- Get products with a certain category
+CREATE PROC sp_GetProductByCategory @CategoryID int
+AS
+SELECT [ProductName],[UnitPrice],[Picture],ProductID FROM dbo.tblProduct
+JOIN dbo.tblCategory
+ON dbo.tblProduct.CategoryID = dbo.tblCategory.CategoryID
+WHERE dbo.tblCategory.CategoryID=@CategoryID
+GO
+
+exec sp_GetProductByCategory 2
+go
+
+-- Insert to tblGuestFeedBack
+CREATE PROC sp_GetDetailProductByProductID @ProductID int
+AS
+SELECT * FROM dbo.tblProduct WHERE ProductID = @ProductID
+GO
+EXEC sp_GetDetailProductByProductID 1
+GO
+-- Insert feebacks into the table tblGuestFeedBack
+CREATE PROC sp_Add2GuestFeedBack @Email varchar(100),@FullName varchar(100),@CompanyName varchar(100),
+@Address varchar(50),@City varchar(50),@Nation varchar(50),@PhoneNumber varchar(50),
+@PostalCode varchar(20),@Subject varchar(100),@Comment varchar(500)
+AS
+insert into tblGuestFeedBack(Email,FullName,CompanyName,[Address],City,Nation,
+PhoneNumber,PostalCode,[Subject],Comment) values(@Email,@FullName,@CompanyName,@Address,@City,@Nation,
+@PhoneNumber,@PostalCode,@Subject,@Comment) 
+GO
+-- Update the mostFrequentlyViewed of a product
+CREATE PROC sp_UpdateViewedProduct @ProductID int,@FrequentlyViewed int
+AS
+update tblProduct set FrequentlyViewed=@FrequentlyViewed where ProductID=@ProductID
+GO
+exec sp_UpdateViewedProduct 1,5
+go
+
+
